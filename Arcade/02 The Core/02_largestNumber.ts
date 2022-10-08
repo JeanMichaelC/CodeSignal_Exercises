fd6b7831193f4fd21a98784c2851100cf1d346bd
JeanMichaelC <JeanMichaelC@randomemail.com>
2022-10-08 19:59:33 +0200

    :memo: Added Exercise 02
    Solved in 2 minutes, I found it somewhat funny

---
+++ Arcade/02 The Core/02_largestNumber.ts
@@ -0,0 +1,12 @@
+(() => {
+	// My solution
+	function solution(n: number): number {
+		return Number('9'.repeat(n));
+	}
+
+	// Great solutions of others
+	function nissim_l(n: number): number {
+    return Math.pow(10, n) - 1
+}
+})
+
